599cf0d979ce8cc2927300afd20adc410d8777fa
sergeydoma <sergeydoma@gmail.com>
2024-03-03 20:23:09 +0300

    Слияние

---
+++ TST_ABSTRACT.py
@@ -0,0 +1,113 @@
+import sys
+from multiprocessing import Process
+from multiprocessing.managers import BaseManager
+
+import numpy as np
+from PySide6.QtCore import QAbstractTableModel, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableView
+
+
+class PandasModel(QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole or role == Qt.EditRole:
+                value = self._data[index.row(), index.column()]
+                return str(value)
+
+    def setData(self, index, value, role):
+        if role == Qt.EditRole:
+            try:
+                value = int(value)
+            except ValueError:
+                return False
+            self._data[index.row(), index.column()] = value
+            return True
+        return False
+
+    def flags(self, index):
+        return Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsEditable
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.table = QTableView()
+
+        data = np.array([
+          [1, 9, 2],
+          [1, 0, -1],
+          [3, 5, 2],
+          [3, 3, 2],
+          [5, 8, 9],
+        ])
+
+        self.model = PandasModel(data)
+        self.table.setModel(self.model)
+
+        self.setCentralWidget(self.table)
+
+class CustomManager (BaseManager):
+    # nothing
+    pass
+    def ArrayHelper(self, param):
+        pass
+
+class ArrayHelper ():
+    def __init__(self, dim):
+        self.array = np.ones ( dim )
+
+    # access array data by slice
+    def getdata(self, array_slice):
+        return self.array[array_slice]
+
+    def setdata(self, array_slice, value):
+        self.array[array_slice] = value
+
+    def setOnedata(self, row, col, value):
+        self.array[row][col] = value
+
+    # call functions on the numpy array
+    def sum(self):
+        return self.array.sum ()
+
+def task(data_proxy):
+    for i in range ( 1 ):
+        # report details of the array
+        print ( f'Array sum (in child): {data_proxy.sum ()}' )
+        data_proxy.setOnedata ( 1, 3, 803 )
+        print ( 'Tack', data_proxy.getdata ( slice ( 0, 10 ) ) )
+
+    # if 1 == 1:
+    CustomManager.register ( 'ArrayHelper', ArrayHelper )
+        # create and start the custom manager
+    with CustomManager () as manager:
+        data_proxy = manager.ArrayHelper ( (10, 10) )
+        print ( f'Array created on host: {data_proxy}' )
+        # confirm content
+        print ( f'Array sum: {data_proxy.sum ()}' )
+        # access data in the array
+        # data_proxy.setdata ( slice ( 1, 3 ), 880 )
+        data_proxy.setOnedata ( 1, 3, 403 )
+        print ( data_proxy.getdata ( slice ( 0, 10 ) ) )
+
+        process = Process ( target=task, args=(data_proxy,) )
+
+        process.start ()
+
+        process.join ()
+
+        app = QApplication ( sys.argv )
+        window = MainWindow ()
+        window.show ()
+        app.exec ()
--- main.py
+++ main.py
@@ -1,40 +1,64 @@
 # share a numpy array between processes using a manager
-from multiprocessing import Process
+import sys
+
+import numpy as np
+from PySide6.QtCore import QAbstractTableModel, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableView
+
 from multiprocessing.managers import BaseManager
+from edit_dialog import Ui_MainWindow
 
 import pandas as pd
-from PySide6 import QtCore
+from PySide6 import QtCore, QtWidgets
 from PySide6.QtCore import Qt
 from numpy import ones
 import numpy as np
 
+import sys
+import time
+from threading import Thread
+
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import QAbstractTableModel, Qt
+
+from PySide6.QtGui import QPalette, QColor
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QLineEdit, QTableView
+# from visuPmk import Visu_ui
+import pandas as pd
+from multiprocessing import Process
+# from exchange import process_mb as p_mb
+
 
 #region _ Abstact Model
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
 
-class TableModel ( QtCore.QAbstractTableModel ):
-	def __init__(self, data):
-		super ( TableModel, self ).__init__ ()
-		self._data = data
+    def rowCount(self, index):
+        return self._data.shape[0]
 
-	def data(self, index, role):
-		if role == Qt.DisplayRole:
-			value = self._data.iloc[index.row (), index.column ()]
-			return str ( value )
+    def columnCount(self, index):
+        return self._data.shape[1]
 
-	def rowCount(self, index):
-		return self._data.shape[0]
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole or role == Qt.EditRole:
+                value = self._data[index.row(), index.column()]
+                return str(value)
 
-	def columnCount(self, index):
-		return self._data.shape[1]
+    def setData(self, index, value, role):
+        if role == Qt.EditRole:
+            try:
+                value = int(value)
+            except ValueError:
+                return False
+            self._data[index.row(), index.column()] = value
+            return True
+        return False
 
-	def headerData(self, section, orientation, role):
-		# section is the index of the column/row.
-		if role == Qt.DisplayRole:
-			if orientation == Qt.Horizontal:
-				return str ( self._data.columns[section] )
-			if orientation == Qt.Vertical:
-				return str ( self._data.index[section] )
-
+    def flags(self, index):
+        return Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsEditable
 
 # endregion
 #region DataFrame
@@ -65,8 +89,8 @@ data = [
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-	]
-pd.DataFrame (data, columns=['Калал 1', 'Калал 2', 'Калал 3', 'Калал 4', 'Калал 5',
+    ]
+pd.DataFrame(data, columns=['Калал 1', 'Калал 2', 'Калал 3', 'Калал 4', 'Калал 5',
             'Калал 6', 'Калал 7', 'Калал 8', 'Калал 9', 'Калал 10'],
     index=['Режим работы:', 'Режим работы канала', 'Допустимые диапазоны:', 'Диапазон сопр. изоляции авар.',
            'Диапазон сопр. шлейфа авар.', 'Диапазон сопр. изоляции предупр.', 'Диапазон сопр. шлейфа предупр.',
@@ -77,65 +101,89 @@ pd.DataFrame (data, columns=['Калал 1', 'Калал 2', 'Калал 3', 'К
            'Сопр. изоляции 2 ниже доп.', 'Сопр. шлейфа ниже доп.', 'Сопр. шлейфа выше доп.',
            'Напряжение на входе 1 выше доп.', 'Напряжение на входе 2 выше доп.'])
 #endregion
+#region MAIN
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        # Visu_ui.__init__(self)
+        self.setupUi(self)
+        # self.writeTabl()
+        # self.threadpool = QThreadPool ()
+        # print ( "Multithreading with maximum %d threads" % self.threadpool.maxThreadCount ())
+        self.tableView_Arhive
+
+        self.model = TableModel(data)
+        self.tableView_Arhive.setModel(self.model)
+        self.setCentralWidget(self.tableView_Arhive)
+#endregion
+
 # custom manager to support custom classes
 class CustomManager ( BaseManager ):
-	# nothing
-	pass
+    # nothing
+    pass
 
-	def ArrayHelper(self, param):
-		pass
+    def ArrayHelper(self, param):
+        pass
 
 
 class ArrayHelper ():
-	def __init__(self, dim):
-		self.array = np.ones ( dim )
+    def __init__(self, dim):
+        self.array = np.ones ( dim )
 
-	# access array data by slice
-	def getdata(self, array_slice):
-		return self.array[array_slice]
+    # access array data by slice
+    def getdata(self, array_slice):
+        return self.array[array_slice]
 
-	def setdata(self, array_slice, value):
-		self.array[array_slice] = value
+    def setdata(self, array_slice, value):
+        self.array[array_slice] = value
 
-	def setOnedata(self, row, col, value):
-		self.array[row][col] = value
+    def setOnedata(self, row, col, value):
+        self.array[row][col] = value
 
-	# call functions on the numpy array
-	def sum(self):
-		return self.array.sum ()
+    # call functions on the numpy array
+    def sum(self):
+        return self.array.sum ()
+
+
 
 
 # task executed in a child process
 def task(data_proxy):
-	for i in range ( 1 ):
-		# report details of the array
-		print ( f'Array sum (in child): {data_proxy.sum ()}' )
-		data_proxy.setOnedata ( 1, 3, 803 )
-		print ( 'Tack', data_proxy.getdata ( slice ( 0, 10 ) ) )
+    for i in range ( 1 ):
+        # report details of the array
+        print ( f'Array sum (in child): {data_proxy.sum ()}' )
+        data_proxy.setOnedata ( 1, 3, 803 )
+        print ( 'Tack', data_proxy.getdata ( slice ( 0, 10 ) ) )
 
 
 # protect the entry point
 if __name__ == '__main__':
-	# register the a python class with the custom manager
-	CustomManager.register ( 'ArrayHelper', ArrayHelper )
-	# create and start the custom manager
-	with CustomManager () as manager:
-		# define the size of the numpy array
-		# n = 100000000
-		# create a shared numpy array
-		data_proxy = manager.ArrayHelper ( (10, 10) )
-		print ( f'Array created on host: {data_proxy}' )
-		# confirm content
-		print ( f'Array sum: {data_proxy.sum ()}' )
-		# access data in the array
-		# data_proxy.setdata ( slice ( 1, 3 ), 880 )
-		data_proxy.setOnedata ( 1, 3, 403 )
-		print ( data_proxy.getdata ( slice ( 0, 10 ) ) )
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
 
-		process = Process ( target=task, args=(data_proxy,) )
+    # register the a python class with the custom manager
+    CustomManager.register ( 'ArrayHelper', ArrayHelper )
+    # create and start the custom manager
+    with CustomManager () as manager:
+        # define the size of the numpy array
+        # n = 100000000
+        # create a shared numpy array
+        data_proxy = manager.ArrayHelper ( (10, 10) )
+        print ( f'Array created on host: {data_proxy}' )
+        # confirm content
+        print ( f'Array sum: {data_proxy.sum ()}' )
+        # access data in the array
+        # data_proxy.setdata ( slice ( 1, 3 ), 880 )
+        data_proxy.setOnedata ( 1, 3, 403 )
+        print ( data_proxy.getdata ( slice ( 0, 10 ) ) )
 
-		process.start ()
+        process = Process ( target=task, args=(data_proxy,) )
 
-		process.join ()
+        process.start ()
+
+        process.join ()
 
 # print ( 'Out', data_proxy.getdata(slice(0,10)))
