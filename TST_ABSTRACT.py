9df21af1e59d7d094c9f9c9d02d6887fae6e2ce8
sergeydoma <sergeydoma@gmail.com>
2024-03-03 20:58:41 +0300

    Работает только 1 поток

--- TST_ABSTRACT.py
+++ TST_ABSTRACT.py
@@ -82,32 +82,33 @@ class ArrayHelper ():
         return self.array.sum ()
 
 def task(data_proxy):
-    for i in range ( 1 ):
+    for i in range ( 9 ):
         # report details of the array
         print ( f'Array sum (in child): {data_proxy.sum ()}' )
         data_proxy.setOnedata ( 1, 3, 803 )
         print ( 'Tack', data_proxy.getdata ( slice ( 0, 10 ) ) )
 
     # if 1 == 1:
-    CustomManager.register ( 'ArrayHelper', ArrayHelper )
-        # create and start the custom manager
-    with CustomManager () as manager:
-        data_proxy = manager.ArrayHelper ( (10, 10) )
-        print ( f'Array created on host: {data_proxy}' )
-        # confirm content
-        print ( f'Array sum: {data_proxy.sum ()}' )
-        # access data in the array
-        # data_proxy.setdata ( slice ( 1, 3 ), 880 )
-        data_proxy.setOnedata ( 1, 3, 403 )
-        print ( data_proxy.getdata ( slice ( 0, 10 ) ) )
+CustomManager.register ( 'ArrayHelper', ArrayHelper )
+    # create and start the custom manager
+with CustomManager () as manager:
+    data_proxy = manager.ArrayHelper ( (10, 10) )
+    print ( f'Array created on host: {data_proxy}' )
+    # confirm content
+    print ( f'Array sum: {data_proxy.sum ()}' )
+    # access data in the array
+    # data_proxy.setdata ( slice ( 1, 3 ), 880 )
+    data_proxy.setOnedata ( 1, 3, 403 )
+    print ( data_proxy.getdata ( slice ( 0, 10 ) ) )
 
-        process = Process ( target=task, args=(data_proxy,) )
+    app = QApplication ( sys.argv )
+    window = MainWindow ()
+    window.show ()
+    app.exec ()
 
-        process.start ()
+    process = Process ( target=task, args=(data_proxy,) )
 
-        process.join ()
+    process.start ()
 
-        app = QApplication ( sys.argv )
-        window = MainWindow ()
-        window.show ()
-        app.exec ()
+    process.join ()
+
